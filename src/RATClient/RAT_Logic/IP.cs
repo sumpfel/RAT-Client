6c3039885d279c6a58ca9b306ea1c2d5f6f6c78f
sumpfel <christof.schaffer@schaffer-se.at>
2026-05-31 19:04:25 +0200

    Added validations (Port, IP)

--- src/RATClient/RAT_Logic/IP.cs
+++ src/RATClient/RAT_Logic/IP.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace RAT_Logic
@@ -13,5 +14,27 @@ namespace RAT_Logic
         public string IPv4SubnetMask;
         public int IPv6PrefixLength;
         public string IPv4Gateway;
+
+        public static bool IsIpv4Valid(string ipv4)
+        {
+            if (Regex.IsMatch(ipv4, @"^(((?!25?[6-9])[12]\d|[1-9])?\d\.?\b){4}$")) // Regex from https://stackoverflow.com/questions/5284147/validating-ipv4-addresses-with-regexp
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsPortVlaid(Object port)
+        {
+            try
+            {
+                Convert.ToUInt16(port);
+                return true;
+            }
+            catch {  return false; }
+        }
     }
 }
