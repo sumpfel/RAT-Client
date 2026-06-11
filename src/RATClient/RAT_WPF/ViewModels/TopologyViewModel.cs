fce541e87c8396e63f16dc2eb88225e5a2d927ad
Pir4t3141 <lamara.reich@gmail.com>
2026-06-11 12:18:19 +0200

    ADDED: Binding Radiobuttons in TopBar to Enum ADDED: Command that is called when Clicking on NetworkObjectView and the Selected Tool is the ConnectionTool, when CursorTool it just drags

--- src/RATClient/RAT_WPF/ViewModels/TopologyViewModel.cs
+++ src/RATClient/RAT_WPF/ViewModels/TopologyViewModel.cs
@@ -11,15 +11,35 @@ using RAT_WPF.Views;
 
 namespace RAT_WPF.ViewModels
 {
+    public enum EnumTool
+    {
+        Cursor,
+        Connector
+    };
+
     public class TopologyViewModel : ViewModelBase
     {
         public NetworkObjectListingViewModel defaultItems { get; }
 
         public ICommand NetworkObjectAddedCommand { get; }
 
+        public ICommand NetworkObjectAddConnectionCommand { get; }
+
         private readonly ObservableCollection<NetworkObjectViewModel> _networkObjects;
 
         public IEnumerable<NetworkObjectViewModel> NetworkObjects => _networkObjects;
+
+        private EnumTool _toolEnum = EnumTool.Cursor;
+        public EnumTool ToolEnum
+        {
+            get => _toolEnum;
+
+            set
+            {
+                _toolEnum = value;
+                OnPropertyChanged(nameof(ToolEnum));
+            }
+        }
 
         public TopologyViewModel()
         {
@@ -33,13 +53,14 @@ namespace RAT_WPF.ViewModels
 
             // NetworkObjectAddedCommand = new NetworkObjectAddedCommand(defaultItems.NetworkObjects);
 
+            this.NetworkObjectAddConnectionCommand = new NetworkObjectAddConnectionCommand();
+
             _networkObjects = new ObservableCollection<NetworkObjectViewModel>()
             {
-                /* For testing
+                // For testing
                 
                 new NetworkObjectViewModel(new NetworkObject() { Type = NetworkObjectType.Router , Name = "Test", Settings = new NetworkObjectSettings(), X = 50, Y=100}),
-                new NetworkObjectViewModel(new NetworkObject() { Type = NetworkObjectType.Router , Name = "Test2", Settings = new NetworkObjectSettings(), X = 350, Y=250})
-                */
+                new NetworkObjectViewModel(new NetworkObject() { Type = NetworkObjectType.Router , Name = "Test2", Settings = new NetworkObjectSettings(), X = 350, Y=250})  
             };
         }
 
